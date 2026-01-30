3e60a8a6b3609a20a51323549352ffbbcd7667bf
manny <praisechinonso21@gmail.com>
2026-01-30 09:49:38 +0100

    feat: add sonner for toast notifications

--- web/components/providers.tsx
+++ web/components/providers.tsx
@@ -2,11 +2,13 @@
 
 import * as React from "react";
 import { WalletProvider } from "./wallet-provider";
+import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WalletProvider>
       {children}
+      <Toaster position="bottom-right" richColors />
     </WalletProvider>
   );
 }
